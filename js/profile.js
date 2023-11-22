591649a1cc725a6ae032b35c573660b233e83c20
albingcj <114584909+albingcj@users.noreply.github.com>
2023-11-22 14:28:52 +0530

    file

---
+++ js/profile.js
@@ -0,0 +1,9 @@
+// toggle visibility by the edit button
+function toggleVisibility() {
+    var updForm = document.getElementById("updForm");
+    var detTable = document.getElementById("detTable");
+
+    // toggle visibility
+    updForm.classList.toggle("d-none");
+    detTable.classList.toggle("d-none");
+}
